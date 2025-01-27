d3b10f8cbbefd5284482b82efda0f2eaed286410
Mira Ballan <miraballan14@gmail.com>
2025-01-27 22:43:46 +0200

    v5

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -69,21 +69,31 @@ class _CustomGestureButtonState extends State<CustomGestureButton> {
 
   Future<void> _tapDown(String cmd) async {
     _setPressedState(cmd, true);
+    if (cmd == "V") {
+      sendCmdToServer(context: context, cmd: currCmd);
+      _setPressedState(cmd, false);
+      return;
+    }
     _timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
       currCmd = getCurrCmd(cmd);
 
-      sendCmdToServer(context: context,cmd: currCmd);
+      sendCmdToServer(context: context, cmd: currCmd);
     });
   }
 
   Future<void> _tapUp(String cmd) async {
+    if (cmd == "V") {
+      sendCmdToServer(context: context, cmd: "v");
+      _setPressedState(cmd, false);
+      return;
+    }
     if (_timer != null) {
       _timer!.cancel();
       _timer = null;
     }
     setPressedState(cmd, false);
     if (widget.sendStop) {
-        sendCmdToServer(context: context,cmd: 'S');
+      sendCmdToServer(context: context, cmd: 'S');
     }
   }
 
